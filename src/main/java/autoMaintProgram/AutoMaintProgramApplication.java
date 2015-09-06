4f442c512aa735facbc23735d24f91953f67244b
Justin <justin.safford@gmail.com>
2015-09-06 18:23:56 -0500

    first commit

---
+++ src/main/java/autoMaintProgram/AutoMaintProgramApplication.java
@@ -0,0 +1,12 @@
+package autoMaintProgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMaintProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoMaintProgramApplication.class, args);
+    }
+}
