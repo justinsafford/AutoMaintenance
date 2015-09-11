646fdd7d0cf43b7453297a0dfb9f32b87ed37b5b
Justin <justin.safford@gmail.com>
2015-09-10 21:51:22 -0500

    replacing to original vehicleTest name

--- src/test/java/autoMaintProgram/vehicleControllerTest.java
+++ src/test/java/autoMaintProgram/vehicleControllerTest.java
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
-public class vehicleControllerTest1 {
+public class vehicleControllerTest {
     @Mock
     VehicleRepository vehicleRepository;
 
