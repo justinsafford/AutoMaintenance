7a485922754e8241a2da09f5687155de4ead1b7b
Justin <justin.safford@gmail.com>
2015-09-14 17:47:26 -0500

    restructure vehicle tests

---
+++ src/test/java/autoMaintProgram/controllers/vehicle/addVehicleController_Test.java
@@ -0,0 +1,93 @@
+package autoMaintProgram.controllers.vehicle;
+
+import autoMaintProgram.ResourcesNotFoundException;
+import autoMaintProgram.garage.GarageEntity;
+import autoMaintProgram.repos.GarageRepository;
+import autoMaintProgram.repos.VehicleRepository;
+import autoMaintProgram.vehicle.VehicleController;
+import autoMaintProgram.vehicle.VehicleEntity;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.Is.isA;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class addVehicleController_Test {
+    @Mock
+    VehicleRepository vehicleRepository;
+
+    @Mock
+    GarageRepository garageRepository;
+
+    @InjectMocks
+    VehicleController vehicleController;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    MockMvc mockMvc;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Before
+    public void setupMock() {
+        mockMvc = standaloneSetup(vehicleController)
+                .build();
+    }
+
+    @Test
+    public void addNewVehicleToGarage() throws Exception {
+        GarageEntity garageEntity = new GarageEntity();
+        garageEntity.setGarageId("id");
+        garageEntity.setGarageName("testName");
+
+        GarageEntity expectedGarageEntity = new GarageEntity();
+        when(garageRepository.findOne(garageEntity.getGarageId()))
+                .thenReturn(expectedGarageEntity);
+
+        mockMvc.perform(post("/garages/{garageId}/vehicles", garageEntity.getGarageId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{}"))
+                .andExpect(status().isCreated());
+
+        verify(vehicleRepository, times(1)).save(Matchers.isA(VehicleEntity.class));
+        verifyNoMoreInteractions(vehicleRepository);
+    }
+
+    @Test
+    public void addNewVehicleWithUnknownGarage_throwsRNFException() throws Exception {
+        GarageEntity garageEntity = new GarageEntity();
+        when(garageRepository.findOne(garageEntity.getGarageId())).thenReturn(null);
+
+        expectedException.expectCause(isA(ResourcesNotFoundException.class));
+        expectedException.expectMessage("Garage not found");
+
+        mockMvc.perform(post("/garages/{garageId}/vehicles", "id")
+                .content("{}")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andReturn();
+    }
+
+    @Test
+    public void addNewVehicleWithMissingGarage_throwsRNFException() throws Exception {
+        expectedException.expectCause(isA(ResourcesNotFoundException.class));
+        expectedException.expectMessage("Garage is required");
+
+        mockMvc.perform(post("/garages//vehicles"));
+    }
+}
