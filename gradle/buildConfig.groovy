a1d778eefb11b64d587e485fe36ff5157f0f73e6
Justin <justin.safford@gmail.com>
2015-12-20 01:40:47 -0600

    add environment config for cf

--- gradle/buildConfig.groovy
+++ gradle/buildConfig.groovy
@@ -1,5 +1,20 @@
 environments {
-    local {
 
+    dev {
+        cf {
+            target = 'https://api.run.pivotal.io'
+            space = 'development'
+            uri = 'http://automaintenance-dev.cfapps.io'
+            application = 'AutoMaintenance-dev'
+        }
+    }
+
+    staging {
+        cf {
+            target = 'https://api.run.pivotal.io'
+            space = 'staging'
+            uri = 'http://automaintenance-staging.cfapps.io'
+            application = 'AutoMaintenance-staging'
+        }
     }
 }
